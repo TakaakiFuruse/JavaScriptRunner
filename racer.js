4ee8960f6c6f75b7d9bff0b3143e575e2f7b3277
TakaakiFuruse <takaakifuruse@gmail.com>
2014-12-25 13:06:21 +0900

    all method done start making view method

--- racer.js
+++ racer.js
@@ -1,41 +1,46 @@
 // $("img#player1").animate({'margin-left': '+=0.5%'}, 100);
 // $("img#player2").animate({'margin-left': '+=0.5%'}, 100);
 
-// p-code
+// p-c
 
 // player 1
 // player 2
 
-// push key -> move forward -> goal 
+// push key -> move forward -> goal
 
 //initialize
 
 function player (playerNum) {
   this.image = "img#player" + playerNum;
+  this.flag = "img#flag_p" + playerNum;
   this.win = false;
-};
-
+  this.playerNum = playerNum;
+}
 
 // controller
 player.prototype.move = function() {
   // move image px by px
-  $(this.image).animate({'margin-left': '+=0.5%'}, 100);
+  $(this.image).animate({"margin-left": "+=1%"}, 100);
 };
 
-player.prototype.win = function() {
-  // check win status
+player.prototype.playerGoal = function() {
+  // when player touched flag. He won. 
+  playerPosition = $(this.image).css("margin-left");
+  flagPosition = $(this.flag).css("margin-left");
+  if (playerPosition >= flagPosition){this.win = true;}
 };
 
-player.prototype.goal = function() {
+player.prototype.congratsWinner = function() {
   // display who won
+  if (this.win === true) { 
+  	$(".winner p").append("Player " + this.playerNum +  " WIN!!");
+  }
 };
 
-player.prototype.reset = function(first_argument) {
-  // get players back to start line
-};
+player.prototype.reset = function() {
+   // get players back to start line
+   $(this.image).css("margin-left", "0px");
+ };
 
 
 // view
-$(document).ready(function() {
-  
-});
