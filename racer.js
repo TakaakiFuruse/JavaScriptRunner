cd746b7e64bf1a6014b016e5e59bc691d532b9cf
TakaakiFuruse <takaakifuruse@gmail.com>
2014-12-25 10:04:45 +0900

    add:move method working, inithalize working

--- racer.js
+++ racer.js
@@ -8,6 +8,34 @@
 
 // push key -> move forward -> goal 
 
-function player (argument) {
-  // body...
-}
+//initialize
+
+function player (playerNum) {
+  this.image = "img#player" + playerNum;
+  this.win = false;
+};
+
+
+// controller
+player.prototype.move = function() {
+  // move image px by px
+  $(this.image).animate({'margin-left': '+=0.5%'}, 100);
+};
+
+player.prototype.win = function() {
+  // check win status
+};
+
+player.prototype.goal = function() {
+  // display who won
+};
+
+player.prototype.reset = function(first_argument) {
+  // get players back to start line
+};
+
+
+// view
+$(document).ready(function() {
+  
+});
